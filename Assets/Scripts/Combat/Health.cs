9422563c2a3d225f1af443f033d417c99e5b7204
raulgs93 <raul.garcia.santiago93@gmail.com>
2020-02-29 13:40:35 +0100

    Added death and hit animations

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -9,11 +9,34 @@ namespace RPG.Combat
 
         [SerializeField] float health = 100f;
 
+        bool isAlive = true;
 
+        
         public void TakeDamage(float damage) {
 
             health = Mathf.Max(health - damage, 0);
             print("health");
+
+
+            if (isAlive) {
+                if (health == 0) {
+                    Die();
+                }
+                else {
+                    GetHit();
+                }
+            }
+            
+        }
+
+        public void Die() {
+            GetComponent<Animator>().SetTrigger("die");
+            isAlive = false;
+        }
+
+        public void GetHit() {
+            GetComponent<Animator>().SetTrigger("getHit");
+            
         }
 
     }
