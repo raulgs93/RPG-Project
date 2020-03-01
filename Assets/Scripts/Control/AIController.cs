f398b69760ac8fc759258c663d29dffced807c10
raulgs93 <raul.garcia.santiago93@gmail.com>
2020-03-01 01:06:23 +0100

    AI patrol and suspicion implemented

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,8 +1,11 @@
 ﻿using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace RPG.Control
 {
@@ -13,15 +16,29 @@ namespace RPG.Control
 
 
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 3f;
+        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] float waypointTolerance = 1f;
 
         GameObject player;
         Fighter fighter;
         Health health;
+        Mover mover;
+        ActionScheduler action;
+
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
+        int currentWaypointIndex = 0;
+        
 
         private void Start() {
             player = GameObject.FindWithTag("Player");
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+            action = GetComponent<ActionScheduler>();
+
+            guardPosition = transform.position;
 
         }
 
@@ -31,16 +48,67 @@ namespace RPG.Control
             if (!health.GetIsAlive()) { return; }
 
             if (InAttackDistance(player) && fighter.CanAttack(player)) {
-                fighter.Attack(player.gameObject);
+                AttackBehaviour();
+                timeSinceLastSawPlayer = 0;
+            }
+            else if (timeSinceLastSawPlayer < suspicionTime) {
+                SuspiciousBehaviour();
             }
             else {
-                fighter.Cancel();
+                PatrolBehaviour();
             }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void PatrolBehaviour() {
+
+            Vector3 nextPosition = guardPosition;
+
+
+            if (patrolPath != null) {
+
+                if (AtWaypoint()) {
+                    CycleWaypoint();
+                }
+
+                nextPosition = GetCurrentWaypoint();
+
+            }
+            mover.StartMoveAction(nextPosition);
+        }
+
+        private bool AtWaypoint() {
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distanceToWaypoint < waypointTolerance;
+        }
+
+        private void CycleWaypoint() {
+            currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+        }
+
+        private Vector3 GetCurrentWaypoint() {
+            return patrolPath.GetWaipoint(currentWaypointIndex);
+        }
+
+
+      
+        private void SuspiciousBehaviour() {
+            action.CancelAction();
+        }
+
+        private void AttackBehaviour() {
+            fighter.Attack(player.gameObject);
         }
 
         public bool InAttackDistance(GameObject target) {
             float distanceToPlayer = Vector3.Distance(transform.position, target.transform.position);
             return distanceToPlayer < chaseDistance;
+        }
+
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
         }
 
     }
---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+
+        [SerializeField] float radius = .2f;
+        private void OnDrawGizmos() {
+            for (int i = 0; i < transform.childCount; i++) {
+                Gizmos.DrawSphere(GetWaipoint(i), radius);
+                Gizmos.DrawLine(GetWaipoint(i), GetWaipoint(GetNextIndex(i)));
+            }
+        }
+
+        public Vector3 GetWaipoint(int i) {
+            return transform.GetChild(i).position;
+        }
+
+        public int GetNextIndex(int i) {
+            if (i+1 == transform.childCount) {
+                return 0;
+            }
+
+            return i + 1; 
+        }
+    }
+
+}
