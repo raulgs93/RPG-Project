12125732d0ab7d8ef91b8c81d4e294b2fdf61b93
raulgs93 <raul.garcia.santiago93@gmail.com>
2020-03-03 00:42:33 +0100

    Added scene loaders

---
+++ Assets/Scripts/Core/PortalController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.Core
+{
+    public class PortalController : MonoBehaviour
+    {
+
+        [SerializeField] int sceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.gameObject.CompareTag("Player")) {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+
+}
+
